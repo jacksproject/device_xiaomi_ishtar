07331b6cb549331011556b4b0b3dcdb0c2f7bc84
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-16 10:19:36 +0000

    ishtar: Add custom xiaomi touch AIDL HAL
    Signed-off-by: jacksproject <contact@jackbd.me>
    Co-authored-by: Harshit Jain <reach@harsh1998.dev>
    Change-Id: I9eecd3e14cf60c9fed921d1c695d9079956982f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ TARGET_NO_BOOTLOADER := true
 # HIDL
 DEVICE_MANIFEST_FILE += \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/framework_matrix_ishtar.xml \
     $(DEVICE_PATH)/configs/vintf/framework_matrix_xiaomi.xml
 
 DEVICE_MANIFEST_FILE += \
